57052a8c935937a7da00db68640bf0d1da35625a
Cin💜 Lo <cinlos3382@gmail.com>
2023-06-06 15:22:11 -0300

    se muestra la lista de productos que viene en el json

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,12 +1,10 @@
+import { products } from './mocks/products.json'
+import { Products } from './components/Products'
 import './App.css'
 
 function App() {
-
-
   return (
-    <>
-      <h1>shopping cart</h1>
-    </>
+      <Products products={products}/>
   )
 }
 
