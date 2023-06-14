dcfdd311964aff4150433495084352dff7332604
Cin💜 Lo <cinlos3382@gmail.com>
2023-06-14 11:02:56 -0300

    MPAs multiple page application

--- projects/07-cinlo-router/src/App.jsx
+++ projects/07-cinlo-router/src/App.jsx
@@ -1,10 +1,38 @@
 
+import { useState } from 'react'
 import './App.css'
 
+function HomePage() {
+  return (
+    <>
+      <h1>Home</h1>
+      <p>Esta es una página de ejemplo para crear un React Router desde cero</p>
+      <a href="/about">Ir a sobre nosotros</a>
+    </>
+  )
+}
+
+
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <img src="https://pbs.twimg.com/profile_images/1552781324120797189/4teiG-tX_400x400.jpg" alt="foto cinlo" />
+      <p>Hola! Me llamo CinLo y estoy creando un clon de React Router</p>
+      <a href="/">Ir a la home</a>
+    </>
+  )
+}
+
+
 function App() {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname)
 
   return (
-    <h1> CinLo Router</h1>
+    <main>
+      {currentPath === '/' && <HomePage />}
+      {currentPath === '/about' && <AboutPage />}
+    </main>
   )
 }
 
