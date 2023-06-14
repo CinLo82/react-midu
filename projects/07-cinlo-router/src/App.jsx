b6780bb2fdf9b8a68b8d30f8d455878f8f41f9ea
Cin💜 Lo <cinlos3382@gmail.com>
2023-06-14 11:23:38 -0300

    SPAs single page application

--- projects/07-cinlo-router/src/App.jsx
+++ projects/07-cinlo-router/src/App.jsx
@@ -1,13 +1,20 @@
-
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+
+const NAVIGATION_EVENT = 'pushstate'
+
+function navigate (href) {
+  window.history.pushState({}, '', href)
+  const navigationEvent = new Event(NAVIGATION_EVENT)
+  window.dispatchEvent(navigationEvent)
+}
 
 function HomePage() {
   return (
     <>
       <h1>Home</h1>
-      <p>Esta es una página de ejemplo para crear un React Router desde cero</p>
-      <a href="/about">Ir a sobre nosotros</a>
+         <p>Esta es una página de ejemplo para crear un React Router desde cero</p>
+      <button onClick={() => navigate('/about')}>Ir a sobre nosotros</button>
     </>
   )
 }
@@ -17,9 +24,12 @@ function AboutPage() {
   return (
     <>
       <h1>About</h1>
-      <img src="https://pbs.twimg.com/profile_images/1552781324120797189/4teiG-tX_400x400.jpg" alt="foto cinlo" />
-      <p>Hola! Me llamo CinLo y estoy creando un clon de React Router</p>
-      <a href="/">Ir a la home</a>
+      <div>
+        <img src="https://pbs.twimg.com/profile_images/1552781324120797189/4teiG-tX_400x400.jpg" alt="foto cinlo" />
+        <p>Hola! Me llamo CinLo y estoy creando un clon de React Router</p>
+      </div>
+      <button onClick={() => navigate('/')}>Ir a la home</button>
+      
     </>
   )
 }
@@ -27,6 +37,17 @@ function AboutPage() {
 
 function App() {
   const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname)
+    }
+    window.addEventListener(NAVIGATION_EVENT, onLocationChange)
+
+    return () => {
+      window.removeEventListener(NAVIGATION_EVENT, onLocationChange)
+    }
+  },[])
 
   return (
     <main>
