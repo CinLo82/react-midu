19f0f953dc98290af68735c0184651955c5f76da
Cin💜 Lo <cinlos3382@gmail.com>
2023-06-14 11:55:54 -0300

    navegar entre páginas con el botón de atrás

--- projects/07-cinlo-router/src/App.jsx
+++ projects/07-cinlo-router/src/App.jsx
@@ -1,38 +1,16 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import { EVENTS } from './const '
+import HomePage from './pages/Home'
+import AboutPage from './pages/About'
 
-const NAVIGATION_EVENT = 'pushstate'
 
-function navigate (href) {
+export function navigate (href) {
   window.history.pushState({}, '', href)
-  const navigationEvent = new Event(NAVIGATION_EVENT)
+  const navigationEvent = new Event(EVENTS.PUSHSTATE)
   window.dispatchEvent(navigationEvent)
 }
 
-function HomePage() {
-  return (
-    <>
-      <h1>Home</h1>
-         <p>Esta es una página de ejemplo para crear un React Router desde cero</p>
-      <button onClick={() => navigate('/about')}>Ir a sobre nosotros</button>
-    </>
-  )
-}
-
-
-function AboutPage() {
-  return (
-    <>
-      <h1>About</h1>
-      <div>
-        <img src="https://pbs.twimg.com/profile_images/1552781324120797189/4teiG-tX_400x400.jpg" alt="foto cinlo" />
-        <p>Hola! Me llamo CinLo y estoy creando un clon de React Router</p>
-      </div>
-      <button onClick={() => navigate('/')}>Ir a la home</button>
-      
-    </>
-  )
-}
 
 
 function App() {
@@ -42,10 +20,13 @@ function App() {
     const onLocationChange = () => {
       setCurrentPath(window.location.pathname)
     }
-    window.addEventListener(NAVIGATION_EVENT, onLocationChange)
+    window.addEventListener(EVENTS.PUSHSTATE, onLocationChange)
+    window.addEventListener(EVENTS.POPSTATE, onLocationChange)
 
     return () => {
-      window.removeEventListener(NAVIGATION_EVENT, onLocationChange)
+      window.removeEventListener(EVENTS.PUSHSTATE, onLocationChange)
+    window.removeEventListener(EVENTS.POPSTATE, onLocationChange)
+
     }
   },[])
 
---
+++ projects/07-cinlo-router/src/pages/About.jsx
@@ -0,0 +1,16 @@
+import { navigate } from '../App'
+
+
+export default function AboutPage() {
+    return (
+      <>
+        <h1>About</h1>
+        <div>
+          <img src="https://pbs.twimg.com/profile_images/1552781324120797189/4teiG-tX_400x400.jpg" alt="foto cinlo" />
+          <p>Hola! Me llamo CinLo y estoy creando un clon de React Router</p>
+        </div>
+        <button onClick={() => navigate('/')}>Ir a la home</button>
+        
+      </>
+    )
+  }
