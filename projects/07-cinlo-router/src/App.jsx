6c1e37d999e069351ca170ecb4ed7e88c2fc3d48
Cin💜 Lo <cinlos3382@gmail.com>
2023-06-16 23:48:19 -0300

    Componente <Route /> para hacerlo declarativo

--- projects/07-cinlo-router/src/App.jsx
+++ projects/07-cinlo-router/src/App.jsx
@@ -3,15 +3,13 @@ import HomePage from './pages/Home'
 import AboutPage from './pages/About'
 import Page404 from './pages/Page404'
 import { Router } from './Router'
+import SearchPage from './pages/Search'
+import { Route } from './Route'
 
 const appRoutes = [
   {
-    path: '/',
-    Component: HomePage
-  },
-  {
-    path: '/about',
-    Component: AboutPage
+    path: '/search/:query',
+    Component: SearchPage
   }
 ]
 
@@ -19,8 +17,11 @@ function App() {
 
   return (
     <main>
-      <Router routes={appRoutes} defaultComponent={Page404}
-      />
+      <Router routes={appRoutes} defaultComponent={Page404}>
+        <Route path='/' Component={HomePage} />
+        <Route path='/about' Component={AboutPage} />
+      </Router>
+      
     </main>
   )
 }
